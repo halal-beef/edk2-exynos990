f2e40e309a0700d17f2c5c5d56ae7306896d222d
halal-beef <78730004+halal-beef@users.noreply.github.com>
2024-05-24 18:24:26 +0100

    ci-build.sh: Migrate to android-tools-mkbootimg
    The "mkbootimg" pkg is very outdated and cannot handle newer bootimages correctly

--- ci-build.sh
+++ ci-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 sudo apt update
-sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc mkbootimg
+sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc android-tools-mkbootimg
 curdir="$PWD"
 cd ..
 git clone https://github.com/tianocore/edk2 --recursive -b edk2-stable202302
