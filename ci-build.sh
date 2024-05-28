5f1cd86f60821403c08bd2bdd2601b5595f14c78
halal-beef <78730004+halal-beef@users.noreply.github.com>
2024-05-28 12:05:32 +0100

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 set -e
+
 sudo apt update
-sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc android-tools-mkbootimg
+sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc
+
 curdir="$PWD"
+
+wget https://raw.githubusercontent.com/halal-beef/res/main/mkbootimg
+chmod +x mkbootimg
+
+export PATH=$PATH:$curdir
+
 cd ..
+
 git clone https://github.com/tianocore/edk2 --recursive --depth=1
 git clone https://github.com/tianocore/edk2-platforms.git
+
 cd "$curdir"
+
 ./firstrun.sh
 ./build.sh
